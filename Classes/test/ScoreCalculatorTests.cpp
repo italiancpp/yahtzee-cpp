21f4762f661d3665682a908ab7420df266252864
ilpropheta <marcoarena@email.it>
2014-10-27 19:29:38 +0100

    added ScoreCalculatorTests

---
+++ Classes/test/ScoreCalculatorTests.cpp
@@ -0,0 +1,60 @@
+#include "gtest/gtest.h"
+#include "../Yahtzee/ScoreCalculator.h"
+#include "../Yahtzee/YahtzeeWriter.h"
+#include <vector>
+
+class FakeWriter : public YahtzeeWriter
+{
+	virtual void startTurnFor( size_t player_count, size_t current_turn, size_t const turns_number ) 
+	{
+	}
+
+	virtual void showPotentialScores( ScoreTable scores ) 
+	{
+	}
+
+	virtual void newGameCreated( const std::vector<DicePlayer>& players, size_t totalTurns ) 
+	{
+	}
+
+	virtual void _startTurnFor( DicePlayer& player, size_t currentTurn ) 
+	{
+	}
+
+	virtual void diceRolled( DicePlayer& player, const std::vector<Die>& dice, size_t currentShot, const ScoreTable& scores ) 
+	{
+	}
+
+	virtual void diceRolledNoMoreShots( DicePlayer& player, const std::vector<Die>& dice, size_t currentShot, const ScoreTable& scores ) 
+	{
+	}
+
+	virtual void endTurnFor( DicePlayer& player, const ScoreTable& currentScores, size_t justEndedTurn ) 
+	{
+	}
+
+};
+
+using namespace std;
+
+TEST(ScoreCalculatorTests, on_pair)
+{
+	FakeWriter writer;
+	ScoreCalculator calculator(writer, 6);
+	vector<Die> dice(5, Die(1));
+	dice[1] = 1;
+	dice[2] = 3;
+	dice[3] = 4;
+	dice[4] = 5;
+
+	ScoreTable expectedTable;
+	expectedTable.AssignScoreIfNotAssigned(Scores::one, 2);
+	expectedTable.AssignScoreIfNotAssigned(Scores::three, 3);
+	expectedTable.AssignScoreIfNotAssigned(Scores::four, 4);
+	expectedTable.AssignScoreIfNotAssigned(Scores::five, 5);
+
+	ScoreTable actualTable;
+	calculator.CheckScore(dice, true, actualTable);
+
+	ASSERT_EQ(actualTable, expectedTable);
+}
