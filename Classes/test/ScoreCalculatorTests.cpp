60dff2f0ef865dfbf89c40342a74a81465f2329f
ilpropheta <marcoarena@email.it>
2014-10-27 21:54:52 +0100

    ScoreCalculator under test

--- Classes/test/ScoreCalculatorTests.cpp
+++ Classes/test/ScoreCalculatorTests.cpp
@@ -37,12 +37,42 @@ class FakeWriter : public YahtzeeWriter
 
 using namespace std;
 
-TEST(ScoreCalculatorTests, on_pair)
+class ScoreCalculatorTests : public testing::Test
 {
+public:
+	ScoreCalculatorTests()
+		: calculator(writer, 6), dice(5, Die(1))
+	{
+
+	}
+
+protected:
+	void CheckScoreOnFirstShot(const ScoreTable& expected);
+	void CheckScoreOnShotDifferentFromFirst(const ScoreTable& expectedTable);
+	
 	FakeWriter writer;
-	ScoreCalculator calculator(writer, 6);
+	ScoreCalculator calculator;
+	vector<Die> dice;
+};
+
+void ScoreCalculatorTests::CheckScoreOnFirstShot(const ScoreTable& expectedTable)
+{
+	ScoreTable actualTable;
+	calculator.CheckScore(dice, true, actualTable);
+	ASSERT_EQ(actualTable, expectedTable);
+}
+
+void ScoreCalculatorTests::CheckScoreOnShotDifferentFromFirst(const ScoreTable& expectedTable)
+{
+	ScoreTable actualTable;
+	calculator.CheckScore(dice, false, actualTable);
+	ASSERT_EQ(actualTable, expectedTable);
+}
+
+TEST_F(ScoreCalculatorTests, on_pair)
+{
 	int diceArr[] = {1,1,3,4,5};
-	vector<Die> dice(diceArr, diceArr+5);
+	dice.assign(diceArr, diceArr+5);
 	
 	ScoreTable expectedTable;
 	expectedTable.AssignScoreIfNotAssigned(Scores::one, 2);
@@ -50,8 +80,97 @@ TEST(ScoreCalculatorTests, on_pair)
 	expectedTable.AssignScoreIfNotAssigned(Scores::four, 4);
 	expectedTable.AssignScoreIfNotAssigned(Scores::five, 5);
 
-	ScoreTable actualTable;
-	calculator.CheckScore(dice, true, actualTable);
+	CheckScoreOnFirstShot(expectedTable);
+}
 
-	ASSERT_EQ(actualTable, expectedTable);
+TEST_F(ScoreCalculatorTests, on_tris)
+{
+	int diceArr[] = {1,1,1,4,5};
+	dice.assign(diceArr, diceArr+5);
+
+	ScoreTable expectedTable;
+	expectedTable.AssignScoreIfNotAssigned(Scores::one, 3);
+	expectedTable.AssignScoreIfNotAssigned(Scores::four, 4);
+	expectedTable.AssignScoreIfNotAssigned(Scores::five, 5);
+
+	CheckScoreOnFirstShot(expectedTable);
+}
+
+TEST_F(ScoreCalculatorTests, on_double_pair)
+{
+	int diceArr[] = {1,1,3,3,5};
+	dice.assign(diceArr, diceArr+5);
+
+	ScoreTable expectedTable;
+	expectedTable.AssignScoreIfNotAssigned(Scores::one, 2);
+	expectedTable.AssignScoreIfNotAssigned(Scores::three, 6);
+	expectedTable.AssignScoreIfNotAssigned(Scores::five, 5);
+
+	CheckScoreOnFirstShot(expectedTable);
+}
+
+TEST_F(ScoreCalculatorTests, on_straight)
+{
+	int diceArr[] = {1,2,3,4,5};
+	dice.assign(diceArr, diceArr+5);
+
+	ScoreTable expectedTable;
+	expectedTable.AssignScoreIfNotAssigned(Scores::one, 1);
+	expectedTable.AssignScoreIfNotAssigned(Scores::two, 2);
+	expectedTable.AssignScoreIfNotAssigned(Scores::three, 3);
+	expectedTable.AssignScoreIfNotAssigned(Scores::four, 4);
+	expectedTable.AssignScoreIfNotAssigned(Scores::five, 5);
+	expectedTable.AssignScoreIfNotAssigned(Scores::straight, 25);
+
+	CheckScoreOnFirstShot(expectedTable);
+
+	expectedTable.AssignScoreIfNotAssigned(Scores::straight, 20);
+	CheckScoreOnShotDifferentFromFirst(expectedTable);
+}
+
+TEST_F(ScoreCalculatorTests, on_full)
+{
+	int diceArr[] = {1,1,3,3,3};
+	dice.assign(diceArr, diceArr+5);
+
+	ScoreTable expectedTable;
+	expectedTable.AssignScoreIfNotAssigned(Scores::one, 2);
+	expectedTable.AssignScoreIfNotAssigned(Scores::three, 9);
+	expectedTable.AssignScoreIfNotAssigned(Scores::full, 35);
+
+	CheckScoreOnFirstShot(expectedTable);
+
+	expectedTable.AssignScoreIfNotAssigned(Scores::full, 30);
+	CheckScoreOnShotDifferentFromFirst(expectedTable);
+}
+
+TEST_F(ScoreCalculatorTests, on_poker)
+{
+	int diceArr[] = {1,1,1,1,5};
+	dice.assign(diceArr, diceArr+5);
+
+	ScoreTable expectedTable;
+	expectedTable.AssignScoreIfNotAssigned(Scores::one, 4);
+	expectedTable.AssignScoreIfNotAssigned(Scores::five, 5);
+	expectedTable.AssignScoreIfNotAssigned(Scores::poker, 45);
+
+	CheckScoreOnFirstShot(expectedTable);
+
+	expectedTable.AssignScoreIfNotAssigned(Scores::poker, 40);
+	CheckScoreOnShotDifferentFromFirst(expectedTable);
+}
+
+TEST_F(ScoreCalculatorTests, on_generala)
+{
+	int diceArr[] = {1,1,1,1,1};
+	dice.assign(diceArr, diceArr+5);
+
+	ScoreTable expectedTable;
+	expectedTable.AssignScoreIfNotAssigned(Scores::one, 5);
+	expectedTable.AssignScoreIfNotAssigned(Scores::generala, 55);
+
+	CheckScoreOnFirstShot(expectedTable);
+
+	expectedTable.AssignScoreIfNotAssigned(Scores::generala, 50);
+	CheckScoreOnShotDifferentFromFirst(expectedTable);
 }
